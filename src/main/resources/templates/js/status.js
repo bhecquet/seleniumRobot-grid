3dde09a804485a0df738d881cc8bc7ec5e1b0961
behe <bhecquet.w@free.fr>
2017-03-14 17:12:41 +0100

    Added screenshot to node status

---
+++ src/main/resources/templates/js/status.js
@@ -0,0 +1,4 @@
+function displayNodeStatus(nodeUrl) {
+	var nodeStatusElement = document.getElementById("nodeStatus");
+	nodeStatusElement.innerHTML =  '<iframe src="' + nodeUrl + '/extra/NodeStatusServlet/" width=50% height=600px"></iframe>' 
+}
