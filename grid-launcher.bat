4f9ad635fd19e3ea8b45838cea316f39dd2a20ce
bhecquet <bhecquet@seleniumRobot.com>
2018-09-18 17:28:48 +0200

    launcher takes arguments

--- grid-launcher.bat
+++ grid-launcher.bat
@@ -1,4 +1,4 @@
 :grid
-	start /wait "grid-hub" java -cp seleniumRobot-grid.jar;lib/drivers/* -Xmx2048m com.infotel.seleniumrobot.grid.GridStarter -role hub
+	start /wait "grid-hub" java -cp seleniumRobot-grid.jar;lib/drivers/* -Xmx2048m com.infotel.seleniumrobot.grid.GridStarter %*
 	timeout /t 5
 goto grid
