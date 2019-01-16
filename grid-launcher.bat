5d1fcae454f8c6b04750e0f6e91bef3708366a20
bhecquet <bhecquet@seleniumRobot.com>
2019-01-16 10:13:59 +0100

    issue #4: force killing of process before starting
    this avoids problems with task schedulerwhich doesn't know about the
    grid process started by launcher

--- grid-launcher.bat
+++ grid-launcher.bat
@@ -1,5 +1,6 @@
 title "grid_launcher"
 :grid
-	start /wait "grid-hub" "%JAVA_HOME%\bin\java.exe" -cp seleniumRobot-grid.jar;lib/drivers/* -Xmx2048m com.infotel.seleniumrobot.grid.GridStarter %*
+	taskkill /F /FI "windowtitle eq grid-process"
+	start /wait "grid-process" "%JAVA_HOME%\bin\java.exe" -cp seleniumRobot-grid.jar;lib/drivers/* -Xmx2048m com.infotel.seleniumrobot.grid.GridStarter %*
 	timeout /t 5
 goto grid
