e9e06cb26da86564d734ba9b33d506c3cf6d5702
bhecquet <bhecquet@seleniumRobot.com>
2019-01-17 07:57:43 +0100

    tests for attaching a chrome process

--- src/test/java/com/infotel/seleniumrobot/grid/tests/SeleniumGridClient.java
+++ src/test/java/com/infotel/seleniumrobot/grid/tests/SeleniumGridClient.java
@@ -15,29 +15,48 @@
  */
 package com.infotel.seleniumrobot.grid.tests;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-import com.seleniumtests.browserfactory.SeleniumRobotCapabilityType;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+//import com.seleniumtests.browserfactory.SeleniumRobotCapabilityType;
 
 public class SeleniumGridClient {
 
-	public static void main(String [] args) throws MalformedURLException {
+	public static void main(String [] args) throws IOException {
 		DesiredCapabilities capability = new DesiredCapabilities();
 		capability.setCapability(CapabilityType.BROWSER_NAME, "chrome");
-		capability.setCapability(SeleniumRobotCapabilityType.NODE_TAGS, Arrays.asList("bar"));
+		System.setProperty("webdriver.gecko.driver", "/home/worm/workspace/seleniumRobot-drivers/seleniumRobot-linux-driver/target/classes/drivers/linux/geckodriver");
+//		capability.setCapability(SeleniumRobotCapabilityType.NODE_TAGS, Arrays.asList("bar"));
 //		capability.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
 //		capability.setCapability(MobileCapabilityType.DEVICE_NAME, "Google Nexus 6 - 6.0.0 - API 23 - 1440x2560");
 		
 		capability.setPlatform(Platform.VISTA);
-		WebDriver driver = new RemoteWebDriver(new URL("http://SN782980:4444/wd/hub"), capability);
-		driver.quit();
+		
+		GeckoDriverService gecko = new GeckoDriverService(new File("/home/worm/workspace/seleniumRobot-drivers/seleniumRobot-linux-driver/target/classes/drivers/linux/geckodriver"), 
+				4444, ImmutableList.of("--connect-existing", "--marionette-port=2828"), ImmutableMap.of());
+		
+		WebDriver driver2 = new FirefoxDriver();
+		
+		WebDriver driver = new FirefoxDriver(gecko);
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+		driver.get("http://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("test");
+		
+//		WebDriver driver = new RemoteWebDriver(new URL("http://SN782980:4444/wd/hub"), capability);
+//		driver.quit();
 	}
 }
