f0bae0587eba6f1a839cb546ddbbc058490f755b
bhecquet <bhecquet@seleniumRobot.com>
2019-11-21 11:38:15 +0100

    issue #48: add mobile DriverProvider to allow mobile tests on nodes

---
+++ src/main/java/com/infotel/seleniumrobot/grid/driver/AppiumIOSDriverProvider.java
@@ -0,0 +1,35 @@
+package com.infotel.seleniumrobot.grid.driver;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.server.DriverProvider;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class AppiumIOSDriverProvider extends AppiumDriverProvider implements DriverProvider {
+
+	@Override
+	public Capabilities getProvidedCapabilities() {
+
+		DesiredCapabilities iosCaps = new DesiredCapabilities();
+		iosCaps.setCapability(MobileCapabilityType.PLATFORM_NAME, "ios");
+		iosCaps.setCapability(CapabilityType.PLATFORM, Platform.MAC);
+
+		return iosCaps;
+	}
+
+	@Override
+	public boolean canCreateDriverInstanceFor(Capabilities capabilities) {
+	    return "ios".equals(capabilities.getCapability(MobileCapabilityType.PLATFORM_NAME));
+	}
+
+	@Override
+	protected Class<? extends WebDriver> getDriverClass() {
+		return IOSDriver.class;
+	}
+
+}
