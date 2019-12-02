8962b410a600d75d5881471934e21febe5416e22
bhecquet <bhecquet.w@free.fr>
2019-12-02 08:33:54 +0100

    issue #52: start minimized

--- grid-launcher.bat
+++ grid-launcher.bat
@@ -1,6 +1,6 @@
 title "grid_launcher"
 :grid
 	taskkill /F /FI "windowtitle eq grid-process"
-	start /wait "grid-process" "%JAVA_HOME%\bin\java.exe" -cp seleniumRobot-grid.jar;lib/drivers/* -Xmx2048m com.infotel.seleniumrobot.grid.GridStarter %*
+	start /MIN /wait "grid-process" "%JAVA_HOME%\bin\java.exe" -cp seleniumRobot-grid.jar;lib/drivers/* -Xmx2048m com.infotel.seleniumrobot.grid.GridStarter %*
 	timeout /t 5
 goto grid
