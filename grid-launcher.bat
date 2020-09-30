24eb15251a8a31ce4059fc6b244eddebe6b57268
bhecquet <bhecquet@seleniumRobot.com>
2020-09-30 10:29:17 +0200

    issue #61: add emulator starting

--- grid-launcher.bat
+++ grid-launcher.bat
@@ -1,4 +1,7 @@
 title "grid_launcher"
+
+for /f "delims=" %%i in ('dir /s /b /a-d "emulator*.bat"') do (start /MIN %%i && timeout /t 30)
+
 :grid
 	taskkill /F /FI "windowtitle eq grid-process"
 	start /MIN /wait "grid-process" "%JAVA_HOME%\bin\java.exe" -cp seleniumRobot-grid.jar;lib/drivers/* -Xmx2048m com.infotel.seleniumrobot.grid.GridStarter %*
