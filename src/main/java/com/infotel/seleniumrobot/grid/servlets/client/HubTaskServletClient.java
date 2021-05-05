787b01fc1bd08180b6f3835647c0539246b65f4e
bhecquet <bhecquet@seleniumRobot.com>
2021-05-05 10:05:39 +0200

    issue #67: add timeout to "executeCommand" method
    maintains session alive by calling current URL on driver

---
+++ src/main/java/com/infotel/seleniumrobot/grid/servlets/client/HubTaskServletClient.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2017 www.infotel.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.infotel.seleniumrobot.grid.servlets.client;
+
+import org.apache.http.HttpHost;
+import org.apache.log4j.Logger;
+
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
+
+public class HubTaskServletClient {
+	
+	private final HttpHost httpHost;
+	
+	private static final String SERVLET_PATH = "/grid/admin/HubTaskServlet";
+	private static final Logger logger = Logger.getLogger(HubTaskServletClient.class.getName());
+	
+	public HubTaskServletClient(String host, int port) {
+		httpHost = new HttpHost(host, port);
+	}
+
+
+	/**
+	 * disable timeout for the session
+	 * Useful when executing long commands
+	 * @throws UnirestException
+	 */
+	public void disableTimeout(String session) throws UnirestException {
+		Unirest.post(String.format("%s%s", httpHost.toURI().toString(), SERVLET_PATH))
+				.queryString("action", "ignoreTimeout")
+				.queryString("ignore", "true")
+				.queryString("session", session)
+				.asString();
+	}
+	
+	/**
+	 * disable timeout for the session
+	 * Useful when executing long commands
+	 * @throws UnirestException
+	 */
+	public void enableTimeout(String session) throws UnirestException {
+		Unirest.post(String.format("%s%s", httpHost.toURI().toString(), SERVLET_PATH))
+		.queryString("action", "ignoreTimeout")
+		.queryString("ignore", "false")
+		.queryString("session", session)
+		.asString();
+	}
+	
+	public void keepDriverAlive(String session) throws UnirestException {
+		Unirest.get(String.format("%s/wd/hub/session/%s/url", httpHost.toURI().toString(), session))
+		.asString();
+	}
+	
+}
--- src/main/java/com/infotel/seleniumrobot/grid/tasks/CommandTask.java
+++ src/main/java/com/infotel/seleniumrobot/grid/tasks/CommandTask.java
@@ -1,30 +1,42 @@
 package com.infotel.seleniumrobot.grid.tasks;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import com.infotel.seleniumrobot.grid.config.LaunchConfig;
 import com.infotel.seleniumrobot.grid.exceptions.TaskException;
 import com.infotel.seleniumrobot.grid.servlets.server.NodeTaskServlet;
 import com.seleniumtests.util.osutility.OSCommand;
-import com.seleniumtests.util.osutility.OSUtility;
 
 public class CommandTask implements Task {
 	
 	private static final Logger logger = Logger.getLogger(NodeTaskServlet.class);
-	private static final List<String> WINDOWS_COMMAND_WHITE_LIST = Arrays.asList("echo", "cmdkey");
-	private static final List<String> LINUX_COMMAND_WHITE_LIST = Arrays.asList("echo");
-	private static final List<String> MAC_COMMAND_WHITE_LIST = Arrays.asList("echo");
+	private static final int DEFAULT_TIMEOUT = 30;
 	
 	private String command = "";
 	private String result = "";
+	private int timeout = 30;
 	private List<String> args = new ArrayList<String>();
 	
+	public static CommandTask getInstance() {
+		return new CommandTask();
+
+	}
+	
 	public void setCommand(String command, List<String> args) {
+		setCommand(command, args, null);
+	}
+	
+	public void setCommand(String command, List<String> args, Integer timeout) {
 		this.command = command;
 		this.args = args;
+		if (timeout == null) {
+			this.timeout = DEFAULT_TIMEOUT;
+		} else {
+			this.timeout = timeout;
+		}
 	}
 	
 	@Override
@@ -32,12 +44,10 @@ public class CommandTask implements Task {
 		result = "";
 		if (command == null || command.isEmpty()) {
 			throw new TaskException("No command provided");
-		} else if (OSUtility.isLinux() && LINUX_COMMAND_WHITE_LIST.contains(command)
-					|| OSUtility.isWindows() && WINDOWS_COMMAND_WHITE_LIST.contains(command)
-					|| OSUtility.isMac() && MAC_COMMAND_WHITE_LIST.contains(command)) {
+		} else if (LaunchConfig.getCurrentLaunchConfig().getExternalProgramWhiteList().contains(command)) {
 			logger.error(String.format("Executing command %s", command));
 			args.add(0, command);
-			result = OSCommand.executeCommandAndWait(args.toArray(new String[] {}));
+			result = OSCommand.executeCommandAndWait(args.toArray(new String[] {}), timeout, null);
 		} else {
 			throw new TaskException(String.format("Command %s is not supported", command));
 		}
