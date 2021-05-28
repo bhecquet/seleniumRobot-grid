e6d680be12a009df8b7b83ed0e6fcae85034c1a4
bhecquet <bhecquet.w@free.fr>
2021-05-28 15:47:01 +0200

    issue #69: add mouse position on captured screenshot

--- src/main/java/com/infotel/seleniumrobot/grid/tasks/ScreenshotTask.java
+++ src/main/java/com/infotel/seleniumrobot/grid/tasks/ScreenshotTask.java
@@ -1,8 +1,14 @@
 package com.infotel.seleniumrobot.grid.tasks;
 
 import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.HeadlessException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.image.BufferedImage;
@@ -41,16 +47,35 @@ public class ScreenshotTask implements Task {
 		}
 	
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		GraphicsDevice defaultGraphicDevice = ge.getDefaultScreenDevice();
-		width = defaultGraphicDevice.getDisplayMode().getWidth();
-		height = defaultGraphicDevice.getDisplayMode().getHeight();
+//		GraphicsDevice defaultGraphicDevice = ge.getDefaultScreenDevice();
+//		width = defaultGraphicDevice.getDisplayMode().getWidth();
+//		height = defaultGraphicDevice.getDisplayMode().getHeight();
+//		
+//		
+		Rectangle screenRect = new Rectangle(0, 0, 0, 0);
+		for (GraphicsDevice gd : ge.getScreenDevices()) {
+		    screenRect = screenRect.union(gd.getDefaultConfiguration().getBounds());
+		}
 		
 		// Capture the screen shot of the area of the screen defined by the rectangle
         BufferedImage bi;
 		try {
-			bi = new Robot().createScreenCapture(new Rectangle(width, height));
+			bi = new Robot().createScreenCapture(screenRect);
+			PointerInfo pointerInfo = MouseInfo.getPointerInfo();
+			Point mouseCoords = pointerInfo.getLocation();
+			mouseCoords.setLocation(mouseCoords.x - screenRect.x, mouseCoords.y - screenRect.y);
+			
+			Graphics graphic = bi.getGraphics();
+			graphic.setColor(Color.red);
+			graphic.drawLine(mouseCoords.x, mouseCoords.y, mouseCoords.x, mouseCoords.y + 12);
+			graphic.drawLine(mouseCoords.x, mouseCoords.y, mouseCoords.x + 10, mouseCoords.y + 10);
+			graphic.drawLine(mouseCoords.x, mouseCoords.y, mouseCoords.x + 8, mouseCoords.y + 17);
+			graphic.dispose();
+			
+			
+			
 			return imgToBase64String(bi, "png");
-		} catch (AWTException e) {
+		} catch (AWTException | HeadlessException e) {
 			logger.warn("Cannot capture image", e);
 			return null;
 		}
