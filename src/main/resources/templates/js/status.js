1981c0f00af9a58c1ef35ee114b7b68f78c2c338
bhecquet <bhecquet.w@free.fr>
2022-06-28 16:23:03 +0200

    #78: more files

--- src/main/resources/templates/js/status.js
+++ src/main/resources/templates/js/status.js
@@ -1,4 +1,4 @@
 function displayNodeStatus(nodeUrl) {
 	var nodeStatusElement = document.getElementById("nodeStatus");
-	nodeStatusElement.innerHTML =  '<iframe src="' + nodeUrl + '/extra/NodeStatusServlet/" width=50% height=600px"></iframe>' 
+	nodeStatusElement.innerHTML =  '<iframe src="' + nodeUrl + '/extra/NodeStatusServlet" width=50% height=600px"></iframe>'
 }
