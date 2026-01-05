1c33dfb2dd634f570525b00e7842d158bab90c72
bhecquet <bhecquet.w@free.fr>
2026-01-05 16:31:56 +0100

    #116: prevent stopping from being done while one is already running

--- src/main/java/com/infotel/seleniumrobot/grid/tasks/video/StopVideoCaptureTask.java
+++ src/main/java/com/infotel/seleniumrobot/grid/tasks/video/StopVideoCaptureTask.java
@@ -15,10 +15,11 @@ import java.util.stream.Collectors;
 
 public class StopVideoCaptureTask extends VideoCaptureTask {
 
+    private static final Object lock = new Object();
     private static final Logger logger = LogManager.getLogger(StopVideoCaptureTask.class);
 
     private static Map<String, File> recordedFiles = Collections.synchronizedMap(new HashMap<>());
-    private String sessionId;
+    private final String sessionId;
     private File videoFile;
 
     public StopVideoCaptureTask(String sessionId) {
@@ -28,40 +29,42 @@ public class StopVideoCaptureTask extends VideoCaptureTask {
     @SuppressWarnings("unchecked")
     @Override
     public StopVideoCaptureTask execute() throws Exception {
-        logger.info("stop video capture for session: {}", sessionId.replaceAll("[\n\r]", "_"));
+        synchronized (lock) {
+            logger.info("stop video capture for session: {}", sessionId.replaceAll("[\n\r]", "_"));
 
-        // check if some files have been cleaned and delete their reference
-        List<String> keysToRemove;
-        synchronized (recordedFiles) {
+            // check if some files have been cleaned and delete their reference
+            List<String> keysToRemove;
+
             keysToRemove = recordedFiles
                     .entrySet()
                     .stream()
                     .filter(entry -> !entry.getValue().exists())
                     .map(e -> e.getKey()).collect(Collectors.toList());
-        }
-        for (String name : keysToRemove) {
-            recordedFiles.remove(name);
-        }
 
-        VideoRecorder recorder = videoRecorders.remove(sessionId);
-        File knownFile = recordedFiles.get(sessionId);
-
-        if (recorder == null) {
-            if (knownFile == null) {
-                return this;
-            } else {
-                videoFile = knownFile;
+            for (String name : keysToRemove) {
+                recordedFiles.remove(name);
             }
-        } else {
-            videoFile = CustomEventFiringWebDriver.stopVideoCapture(DriverMode.LOCAL, null, recorder);
-        }
 
-        if (videoFile != null) {
-            recordedFiles.put(sessionId, videoFile);
-        }
-        logger.info("video capture stopped");
+            VideoRecorder recorder = videoRecorders.remove(sessionId);
+            File knownFile = recordedFiles.get(sessionId);
 
+            if (recorder == null) {
+                if (knownFile == null) {
+                    return this;
+                } else {
+                    videoFile = knownFile;
+                }
+            } else {
+                videoFile = CustomEventFiringWebDriver.stopVideoCapture(DriverMode.LOCAL, null, recorder);
+            }
+
+            if (videoFile != null) {
+                recordedFiles.put(sessionId, videoFile);
+            }
+            logger.info("video capture stopped");
+        }
         return this;
+
     }
 
     public File getVideoFile() {
--- src/test/java/com/infotel/seleniumrobot/grid/tests/tasks/video/TestStopVideoCaptureTask.java
+++ src/test/java/com/infotel/seleniumrobot/grid/tests/tasks/video/TestStopVideoCaptureTask.java
@@ -1,140 +1,201 @@
 package com.infotel.seleniumrobot.grid.tests.tasks.video;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.isNull;
-import static org.mockito.Mockito.*;
-
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import com.infotel.seleniumrobot.grid.config.LaunchConfig;
 import com.infotel.seleniumrobot.grid.tasks.video.StopVideoCaptureTask;
 import com.infotel.seleniumrobot.grid.tasks.video.VideoCaptureTask;
 import com.infotel.seleniumrobot.grid.tests.BaseMockitoTest;
 import com.seleniumtests.driver.CustomEventFiringWebDriver;
 import com.seleniumtests.driver.DriverMode;
+import com.seleniumtests.util.helper.WaitHelper;
 import com.seleniumtests.util.video.VideoRecorder;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
 
 public class TestStopVideoCaptureTask extends BaseMockitoTest {
 
-	@Mock
-	private VideoRecorder recorder;
+    @Mock
+    private VideoRecorder recorder;
 
-	private MockedStatic mockedCustomWebDriver;
-	
-	
-	@BeforeMethod(groups={"grid"})
-	private void setup() {
-		mockedCustomWebDriver = mockStatic(CustomEventFiringWebDriver.class);
+    @BeforeMethod(groups = {"grid"})
+    private void setup() {
 
-		new LaunchConfig(new String[] {"node"});
-		VideoCaptureTask.resetVideoRecorders();
+        new LaunchConfig(new String[]{"node"});
+        VideoCaptureTask.resetVideoRecorders();
 
-	}
+    }
 
-	@AfterMethod(groups = "grid", alwaysRun = true)
-	private void closeMocks() {
-		mockedCustomWebDriver.close();
-	}
-	
-	@Test(groups= {"grid"})
-	public void testStopVideoCapture() throws Exception {
-		// simulate a running recording
-		StopVideoCaptureTask.addVideoRecorder("1234", recorder);
+    @Test(groups = {"grid"})
+    public void testStopVideoCapture() throws Exception {
+        try (MockedStatic<CustomEventFiringWebDriver> mockedCustomWebDriver = mockStatic(CustomEventFiringWebDriver.class)) {
+            // simulate a running recording
+            StopVideoCaptureTask.addVideoRecorder("1234", recorder);
 
-		mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class))).thenReturn(new File("video.avi"));
-		
-		StopVideoCaptureTask task = new StopVideoCaptureTask("1234").execute();
-		Assert.assertEquals(task.getVideoFile(), new File("video.avi"));
+            mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class))).thenReturn(new File("video.avi"));
 
-		mockedCustomWebDriver.verify(() -> CustomEventFiringWebDriver.stopVideoCapture(DriverMode.LOCAL, null, recorder));
-		
-		// recorder has been removed
-		Assert.assertFalse(VideoCaptureTask.getVideoRecorders().containsKey("1234"));
-		
-		// a file has been added
-		Assert.assertTrue(StopVideoCaptureTask.getRecordedFiles().containsKey("1234"));
-		Assert.assertEquals(StopVideoCaptureTask.getRecordedFiles().get("1234"), new File("video.avi"));
-	}
-	
-	/**
-	 * If we call stop several times, when recording is stopped, we get the stored file
-	 * @throws Exception
-	 */
-	@Test(groups= {"grid"})
-	public void testStopVideoCaptureNoRecorder() throws Exception {
+            StopVideoCaptureTask task = new StopVideoCaptureTask("1234").execute();
+            Assert.assertEquals(task.getVideoFile(), new File("video.avi"));
 
-		File videoFile = File.createTempFile("video", ".avi");
-		videoFile.deleteOnExit();
-		
-		Map<String, File> recordedFiles = new HashMap<>();
-		recordedFiles.put("1234", videoFile);
-		
-		// simulate a ended recording (no recorder set)
-		StopVideoCaptureTask.setRecordedFiles(recordedFiles);
+            mockedCustomWebDriver.verify(() -> CustomEventFiringWebDriver.stopVideoCapture(DriverMode.LOCAL, null, recorder));
 
-		mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class))).thenReturn(new File("video.avi"));
-		
-		StopVideoCaptureTask task = new StopVideoCaptureTask("1234").execute();
-		Assert.assertEquals(task.getVideoFile(), videoFile);
+            // recorder has been removed
+            Assert.assertFalse(VideoCaptureTask.getVideoRecorders().containsKey("1234"));
 
-		mockedCustomWebDriver.verify(() -> CustomEventFiringWebDriver.stopVideoCapture(DriverMode.LOCAL, null, recorder), never());
+            // a file has been added
+            Assert.assertTrue(StopVideoCaptureTask.getRecordedFiles().containsKey("1234"));
+            Assert.assertEquals(StopVideoCaptureTask.getRecordedFiles().get("1234"), new File("video.avi"));
+        }
+    }
 
-		// file has been kept
-		Assert.assertTrue(StopVideoCaptureTask.getRecordedFiles().containsKey("1234"));
-		Assert.assertEquals(StopVideoCaptureTask.getRecordedFiles().get("1234"), videoFile);
-	}
-	
-	@Test(groups= {"grid"})
-	public void testStopVideoCaptureNoKnownFile() throws Exception {
+    @Test(groups = {"grid"})
+    public void testStopVideoCaptureSeveralTimes() throws Exception {
 
-		mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class))).thenReturn(new File("video.avi"));
-		
-		StopVideoCaptureTask task = new StopVideoCaptureTask("1234").execute();
-		Assert.assertNull(task.getVideoFile());
+        File videoFile = Files.createTempFile("video", ".avi").toFile();
 
-		mockedCustomWebDriver.verify(() -> CustomEventFiringWebDriver.stopVideoCapture(DriverMode.LOCAL, null, recorder), never());
-		
-		// file has been kept
-		Assert.assertEquals(StopVideoCaptureTask.getRecordedFiles().size(), 0);
-	}
-	
-	/**
-	 * A file exists, check it's removed as it does not exist
-	 * @throws Exception
-	 */
-	@Test(groups= {"grid"})
-	public void testStopVideoCaptureExistingFile() throws Exception {
-		
-		// add 2 files to check for ConcurrentModificationException
-		Map<String, File> recordedFiles = new HashMap<>();
-		recordedFiles.put("bar", new File("oldVideo.avi"));
-		recordedFiles.put("foo", new File("oldVideo2.avi"));
-		
-		// simulate a running recording
-		StopVideoCaptureTask.addVideoRecorder("1234", recorder);
-		StopVideoCaptureTask.setRecordedFiles(recordedFiles);
+        // simulate a running recording
+        StopVideoCaptureTask.addVideoRecorder("1234", recorder);
 
-		mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class))).thenReturn(new File("video.avi"));
-		
-		StopVideoCaptureTask task = new StopVideoCaptureTask("1234").execute();
-		Assert.assertEquals(task.getVideoFile(), new File("video.avi"));
-		
-		// old file has been removed
-		Assert.assertFalse(StopVideoCaptureTask.getRecordedFiles().containsKey("foo"));
-		Assert.assertFalse(StopVideoCaptureTask.getRecordedFiles().containsKey("bar"));
+        // simulate delay in video handling
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-		// a file has been added
-		Assert.assertTrue(StopVideoCaptureTask.getRecordedFiles().containsKey("1234"));
-		Assert.assertEquals(StopVideoCaptureTask.getRecordedFiles().get("1234"), new File("video.avi"));
-	}
+        Map<Integer, File> recordedFiles = new HashMap<>();
+
+        // first call
+        executorService.submit(() -> {
+
+            StopVideoCaptureTask task = null;
+            try (MockedStatic<CustomEventFiringWebDriver> mockedCustomWebDriver = mockStatic(CustomEventFiringWebDriver.class)) {
+                mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class)))
+                        .thenAnswer(invocation -> {
+                            WaitHelper.waitForSeconds(4);
+                            return videoFile;
+                        });
+                task = new StopVideoCaptureTask("1234").execute();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            recordedFiles.put(1, task.getVideoFile());
+        });
+
+        // stop a second time
+        WaitHelper.waitForSeconds(1);
+        executorService.submit(() -> {
+            StopVideoCaptureTask task = null;
+            try (MockedStatic<CustomEventFiringWebDriver> mockedCustomWebDriver = mockStatic(CustomEventFiringWebDriver.class)) {
+                mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class))).thenReturn(videoFile);
+
+                task = new StopVideoCaptureTask("1234").execute();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            recordedFiles.put(2, task.getVideoFile());
+        });
+
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+        }
+        // recorder has been removed
+        Assert.assertFalse(VideoCaptureTask.getVideoRecorders().containsKey("1234"));
+
+        // a file has been added
+        Assert.assertTrue(StopVideoCaptureTask.getRecordedFiles().containsKey("1234"));
+        Assert.assertEquals(StopVideoCaptureTask.getRecordedFiles().get("1234"), videoFile);
+
+        // check each call get the video file
+        Assert.assertEquals(recordedFiles.getOrDefault(1, null), videoFile, "Video file not present");
+        Assert.assertEquals(recordedFiles.getOrDefault(2, null), videoFile, "Video file2 not present");
+
+    }
+
+    /**
+     * If we call stop several times, when recording is stopped, we get the stored file
+     */
+    @Test(groups = {"grid"})
+    public void testStopVideoCaptureNoRecorder() throws Exception {
+        try (MockedStatic<CustomEventFiringWebDriver> mockedCustomWebDriver = mockStatic(CustomEventFiringWebDriver.class)) {
+            File videoFile = File.createTempFile("video", ".avi");
+            videoFile.deleteOnExit();
+
+            Map<String, File> recordedFiles = new HashMap<>();
+            recordedFiles.put("1234", videoFile);
+
+            // simulate a ended recording (no recorder set)
+            StopVideoCaptureTask.setRecordedFiles(recordedFiles);
+
+            mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class))).thenReturn(new File("video.avi"));
+
+            StopVideoCaptureTask task = new StopVideoCaptureTask("1234").execute();
+            Assert.assertEquals(task.getVideoFile(), videoFile);
+
+            mockedCustomWebDriver.verify(() -> CustomEventFiringWebDriver.stopVideoCapture(DriverMode.LOCAL, null, recorder), never());
+
+            // file has been kept
+            Assert.assertTrue(StopVideoCaptureTask.getRecordedFiles().containsKey("1234"));
+            Assert.assertEquals(StopVideoCaptureTask.getRecordedFiles().get("1234"), videoFile);
+        }
+    }
+
+    @Test(groups = {"grid"})
+    public void testStopVideoCaptureNoKnownFile() throws Exception {
+        try (MockedStatic<CustomEventFiringWebDriver> mockedCustomWebDriver = mockStatic(CustomEventFiringWebDriver.class)) {
+            mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class))).thenReturn(new File("video.avi"));
+
+            StopVideoCaptureTask task = new StopVideoCaptureTask("1234").execute();
+            Assert.assertNull(task.getVideoFile());
+
+            mockedCustomWebDriver.verify(() -> CustomEventFiringWebDriver.stopVideoCapture(DriverMode.LOCAL, null, recorder), never());
+
+            // file has been kept
+            Assert.assertEquals(StopVideoCaptureTask.getRecordedFiles().size(), 0);
+        }
+    }
+
+    /**
+     * A file exists, check it's removed as it does not exist on file system
+     */
+    @Test(groups = {"grid"})
+    public void testStopVideoCaptureExistingFile() throws Exception {
+        try (MockedStatic<CustomEventFiringWebDriver> mockedCustomWebDriver = mockStatic(CustomEventFiringWebDriver.class)) {
+            // add 2 files to check for ConcurrentModificationException
+            Map<String, File> recordedFiles = new HashMap<>();
+            recordedFiles.put("bar", new File("oldVideo.avi"));
+            recordedFiles.put("foo", new File("oldVideo2.avi"));
+
+            // simulate a running recording
+            StopVideoCaptureTask.addVideoRecorder("1234", recorder);
+            StopVideoCaptureTask.setRecordedFiles(recordedFiles);
+
+            mockedCustomWebDriver.when(() -> CustomEventFiringWebDriver.stopVideoCapture(any(DriverMode.class), isNull(), any(VideoRecorder.class))).thenReturn(new File("video.avi"));
+
+            StopVideoCaptureTask task = new StopVideoCaptureTask("1234").execute();
+            Assert.assertEquals(task.getVideoFile(), new File("video.avi"));
+
+            // old file has been removed
+            Assert.assertFalse(StopVideoCaptureTask.getRecordedFiles().containsKey("foo"));
+            Assert.assertFalse(StopVideoCaptureTask.getRecordedFiles().containsKey("bar"));
+
+            // a file has been added
+            Assert.assertTrue(StopVideoCaptureTask.getRecordedFiles().containsKey("1234"));
+            Assert.assertEquals(StopVideoCaptureTask.getRecordedFiles().get("1234"), new File("video.avi"));
+        }
+    }
 
 }
